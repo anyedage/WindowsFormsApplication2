ad359e1b9c4812ce9804b56b4720616aee8af229
anyedage <273405460@qq.com>
2016-04-16 07:27:19 +0800

    WindowsFormsApplication2
    WindowsFormsApplication2

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGongZi = new System.Windows.Forms.Button();
+            this.txtOrigin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblDetail = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.btnOrgin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "当月真实工资：";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(211, 58);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 21);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(211, 92);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 21);
+            this.txt2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "当月修改后工资：";
+            // 
+            // btnGongZi
+            // 
+            this.btnGongZi.Location = new System.Drawing.Point(370, 58);
+            this.btnGongZi.Name = "btnGongZi";
+            this.btnGongZi.Size = new System.Drawing.Size(75, 55);
+            this.btnGongZi.TabIndex = 4;
+            this.btnGongZi.Text = "发工资";
+            this.btnGongZi.UseVisualStyleBackColor = true;
+            this.btnGongZi.Click += new System.EventHandler(this.btnGongZi_Click);
+            // 
+            // txtOrigin
+            // 
+            this.txtOrigin.Location = new System.Drawing.Point(151, 20);
+            this.txtOrigin.Name = "txtOrigin";
+            this.txtOrigin.Size = new System.Drawing.Size(100, 21);
+            this.txtOrigin.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "初始值：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "发工资：";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(106, 144);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 21);
+            this.txtInput.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "收入：";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(106, 182);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(100, 21);
+            this.txtOutput.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "指出：";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(42, 237);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 12);
+            this.lblTotal.TabIndex = 12;
+            // 
+            // lblDetail
+            // 
+            this.lblDetail.AutoSize = true;
+            this.lblDetail.Location = new System.Drawing.Point(44, 274);
+            this.lblDetail.Name = "lblDetail";
+            this.lblDetail.Size = new System.Drawing.Size(0, 12);
+            this.lblDetail.TabIndex = 13;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(286, 142);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(71, 23);
+            this.btnInput.TabIndex = 14;
+            this.btnInput.Text = "收入";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(286, 182);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(71, 21);
+            this.btnOutput.TabIndex = 15;
+            this.btnOutput.Text = "支出";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // btnOrgin
+            // 
+            this.btnOrgin.Location = new System.Drawing.Point(274, 20);
+            this.btnOrgin.Name = "btnOrgin";
+            this.btnOrgin.Size = new System.Drawing.Size(75, 23);
+            this.btnOrgin.TabIndex = 16;
+            this.btnOrgin.Text = "设定";
+            this.btnOrgin.UseVisualStyleBackColor = true;
+            this.btnOrgin.Click += new System.EventHandler(this.btnOrgin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 533);
+            this.Controls.Add(this.btnOrgin);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.lblDetail);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOrigin);
+            this.Controls.Add(this.btnGongZi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGongZi;
+        private System.Windows.Forms.TextBox txtOrigin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblDetail;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.Button btnOrgin;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double OriginVal = 0;
+        private double CurrentVal = 0;
+
+
+        private void btnOrgin_Click(object sender, EventArgs e)
+        {
+            CurrentVal = OriginVal = double.Parse(txtOrigin.Text);
+
+            lblTotal.Text = "初始值：" + OriginVal + "\r\n";
+            lblDetail.Text = "初始值：" + CurrentVal + "\r\n";
+        }
+
+        private void btnGongZi_Click(object sender, EventArgs e)
+        {
+            var p1 = double.Parse(txt1.Text);
+            var f = 4630.1 + 200 - p1;
+
+            double v = Math.Round((8721.40 + f * 2 - 0.13), 2);
+            txt2.Text = v.ToString();
+
+            display(v);
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            var v = double.Parse(txtInput.Text);
+            
+            display(v);
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            var v = double.Parse(txtOutput.Text)*(-1);
+            
+            display(v);
+        }
+        private void display(double addVal)
+        {
+            CurrentVal += addVal;
+            lblTotal.Text = CurrentVal.ToString();
+            lblDetail.Text += string.Format("{0}{1}元，余额{2}\r\n", addVal > 0 ? "收入" : "支出", Math.Abs(addVal), CurrentVal);
+        }
+    }
+}
